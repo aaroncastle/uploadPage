ae8749e774b759f5c0630fe538e89e20c64a9467
aaroncastle <aaroncastle@163.com>
2020-12-23 16:38:07 +0800

    delete testfile

--- src/remove.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by AaronCastle on 2020/10/21
- **/
-const fs = require('fs');
-const path = require('path');
-fs.unlink(path.resolve(__dirname,'./sub/aco'),() => {
-    console.log('remove file success')
-});
